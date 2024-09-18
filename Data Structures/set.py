9fc680949a22617f30e20d0c52a3b561f4e5872c
NandhiniMohanasundaram <159524526+NandhiniMohanasundaram@users.noreply.github.com>
2024-09-18 18:04:03 +0530

    Update set.py

--- Data Structures/set.py
+++ Data Structures/set.py
@@ -1,8 +1,57 @@
 #set possible operation
+1.pop()
+2.discard()
+3.remove(ele)
+4. clear() 
+5.copy()
+6.len(iterables)
+7.add()
+
 
 my_set1={'b',1,4,3,'v'}
 my_set2={'r',4,'y'}
+print(my_set1)     //output:{1,3,4,'b','v'}
+
+"""
+# pop()-unlike list ,set will remove random element from the given set and return the value
+my_set1={'b',1,4,3,'v'}
+my_set2={'r',4,'y'}
+
+print(my_set1.pop())  //output:1
+print(my_set1)      //output:{3, 4, 'v', 'b'}
+
+my_set1={'b',1,4,3,'v'}
+
+# discard()
+print(my_set1.discard(1)) //output:None
+print(my_set1)            //output:{3, 4, 'v', 'b'}
+
+# remove()
+my_set1={'b',1,4,3,'v'}
+
+print(my_set1.remove('b'))  //output:None
+print(my_set1)          //output:{1,3, 4, 'v'}
+
+# clear()
+my_set1={'b',1,4,3,'v'}
+my_set2={'r',4,'y'}
+
+print(my_set1.clear())  //output:None
+print(my_set1)          //output:set()
+
+# copy
+print(my_set1.copy())
+
+# len()
+my_set2={4,2,6}
+
+print(len(my_set2))
+
+# add() -instead of append()
+my_set1={1,2,3}
+my_set1.add('z')
 print(my_set1)
+"""
 
 #extend()  ----------as set are unchangeable and hence it does not have extend operation
 # my_set1.extend(my_set2)
